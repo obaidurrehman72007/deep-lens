1acb9a21d5a4f01d0e9cbd1cc34c5917353b65b3
obaidjany-netizen <obaidjany@gmail.com>
2026-02-21 09:59:13 +0500

    Vite updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  base: "/",
   server: {
     proxy: {
       // Directs all /api requests to your Express server
