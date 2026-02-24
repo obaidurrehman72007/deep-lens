72d9e5b13465730ffd3f64b618fd4dffbf8c65f8
obaidjany-netizen <obaidjany@gmail.com>
2026-02-24 08:01:10 +0500

    `Update Vite proxy target to use FRONTEND_URL environment variable`

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     proxy: {
       // Directs all /api requests to your Express server
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.FRONTEND_URL,
         changeOrigin: true,
         secure: false,
       },
