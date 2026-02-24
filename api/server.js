cfc3664c54aafd802664ae66a792d528a6593de0
obaidjany-netizen <obaidjany@gmail.com>
2026-02-24 07:42:49 +0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
-let MongoStore = require('connect-mongo'); // Standard for v4+
+let MongoStore = require('connect-mongo');
 const cors = require('cors');
 const helmet = require('helmet');
 const connectDB = require('./src/config/db');
@@ -12,28 +12,23 @@ const app = express();
 if (MongoStore.default) {
   MongoStore = MongoStore.default;
 }
-
 connectDB();
-
 require('./src/config/passport')(passport);
-
 app.use(helmet());
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   store: MongoStore.create({ 
     mongoUrl: process.env.MONGO_URI,
-    ttl: 14 * 24 * 60 * 60 // session expiration
+    ttl: 14 * 24 * 60 * 60
   }),
   cookie: {
     secure: process.env.NODE_ENV === 'production',
@@ -41,19 +36,13 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000
   }
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use('/api', require('./src/routes/api'));
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-// 2. The "Catch-all" handler:
-// Any request that isn't an API route should serve index.html
 app.get(/^(?!\/api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
-
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
@@ -61,7 +50,6 @@ app.use((err, req, res, next) => {
     stack: process.env.NODE_ENV === 'production' ? null : err.stack
   });
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
